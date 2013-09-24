d26c14f5cda65790c0d741d23da8dccfee05fc50
Samyak Bhuta <samyak.bhuta@gmail.com>
2013-09-24 19:01:52 +0530

    Integration with dotfiles by @holman. Changes in installation script.
    - Appends to the '.zshrc' file if exists, rather then overwriting it after
      backup.
    - May/May not play well with other '.zshrc' setup due to name conflict
      and order of preference in loading the scripts.

--- tools/install.sh
+++ tools/install.sh
@@ -1,3 +1,5 @@
+echo "\033[0;33mOh My Zsh which plays nice with https://github.com/samyakbhuta/dotfiles.\033[0m"
+
 if [ -d ~/.oh-my-zsh ]
 then
   echo "\033[0;33mYou already have Oh My Zsh installed.\033[0m You'll need to remove ~/.oh-my-zsh if you want to install"
@@ -5,7 +7,7 @@ then
 fi
 
 echo "\033[0;34mCloning Oh My Zsh...\033[0m"
-hash git >/dev/null && /usr/bin/env git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh || {
+hash git >/dev/null && /usr/bin/env git clone https://github.com/samyakbhuta/oh-my-zsh.git ~/.oh-my-zsh || {
   echo "git not installed"
   exit
 }
@@ -13,12 +15,15 @@ hash git >/dev/null && /usr/bin/env git clone https://github.com/robbyrussell/oh
 echo "\033[0;34mLooking for an existing zsh config...\033[0m"
 if [ -f ~/.zshrc ] || [ -h ~/.zshrc ]
 then
-  echo "\033[0;33mFound ~/.zshrc.\033[0m \033[0;32mBacking up to ~/.zshrc.pre-oh-my-zsh\033[0m";
-  mv ~/.zshrc ~/.zshrc.pre-oh-my-zsh;
+  echo "\033[0;33mFound ~/.zshrc.\033[0m \033[0;32mAppending to ~/.zshrc the content of Oh My Zsh template file.\033[0m";
+  # mv ~/.zshrc ~/.zshrc.pre-oh-my-zsh;
+  echo "\n\n### Oh-my-zsh\n\n" >> ~/.zshrc
+  cat ~/.oh-my-zsh/templates/zshrc.zsh-template >> ~/.zshrc
+else
+  echo "\033[0;34mUsing the Oh My Zsh template file and adding it to ~/.zshrc\033[0m"
+  cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
 fi
 
-echo "\033[0;34mUsing the Oh My Zsh template file and adding it to ~/.zshrc\033[0m"
-cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
 
 echo "\033[0;34mCopying your current PATH and adding it to the end of ~/.zshrc for you.\033[0m"
 echo "export PATH=\$PATH:$PATH" >> ~/.zshrc
